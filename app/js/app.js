203c523c0e95b8d63a02b43ea1ab0b1715e32c63
Serhii Kornilov <crealime@gmail.com>
2024-04-30 17:44:06 +0300

    Add slider

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,42 @@
 window.addEventListener('load', function () {
+	const body = document.querySelector('body')
+	const header = document.querySelector('.header')
+	const main = document.querySelector('.main')
 	const headerLiExpand = document.querySelector('.header__li-expand')
 	const headerUlLi = document.querySelectorAll('.header__catalog>ul>li')
 	const headerMenu = document.querySelector('.header__menu')
+	const headerMenuBox = document.querySelector('.header__menu-box')
 	const headerCatalog = document.querySelector('.header__catalog')
 	const headerCatalogArr = headerLiExpand.querySelector('.header__catalog i')
 	const headerBurger = document.querySelector('.header__burger')
+	const overlay = document.querySelector('.overlay')
 	const headerBurgerOpen = document.querySelector('.header__burger .fa-bars')
 	const headerBurgerClose = document.querySelector('.header__burger .fa-xmark')
 
+	// Change main margin-top
+	function changeMainMarginTop() {
+		main.style.marginTop = header.offsetHeight + 'px'
+	}
+
+	changeMainMarginTop()
+	window.addEventListener('resize', changeMainMarginTop)
+
 	// Expand mobile menu
-	headerBurger.addEventListener('click', function () {
-		headerMenu.classList.toggle('header__menu_visible')
+	function expandMobileMenu() {
+		headerMenuBox.classList.toggle('header__menu-box_visible')
 		headerBurgerOpen.classList.toggle('d-none')
 		headerBurgerClose.classList.toggle('d-none')
+		overlay.classList.toggle('overlay_visible')
+		body.classList.toggle('body-overflow-y-hidden')
+		// headerMenu.classList.toggle('overflow-y-scroll')
+	}
+
+	headerBurger.addEventListener('click', function () {
+		expandMobileMenu()
+	})
+
+	overlay.addEventListener('click', function () {
+		expandMobileMenu()
 	})
 
 	// Expand ul in mobile menu
@@ -34,6 +58,15 @@ window.addEventListener('load', function () {
 		link.after(clone)
 	});
 
+	// Slider
+	const swiper = new Swiper('.swiper', {
+		loop: true,
+		// autoHeight: true,
+		navigation: {
+			nextEl: '.swiper__button_next',
+			prevEl: '.swiper__button_prev',
+		}
+	})
 
 
 })
